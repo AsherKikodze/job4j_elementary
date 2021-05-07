259887b40388e2c6160cca287f190bbdaf7d9acf
AsherKikodze <asher.kikodze@gmail.com>
2021-05-07 15:27:16 +0300

    Converter

---
+++ src/ru/job4j/condition/SqArea.java
@@ -0,0 +1,15 @@
+package ru.job4j.condition;
+
+public class SqArea {
+    public static double square(int p, double k) {
+        double rs = -1;
+        rs = 0.5 * p / (k + 1);
+        rs = rs * rs * k;
+        return rs;
+    }
+
+    public static void main(String[] args) {
+        double res = SqArea.square(6, 2);
+        System.out.println(" p = 6, k = 2, square = " + res);
+    }
+}
