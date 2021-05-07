a1a4f0b59dbe70406f1d9b4c9c3c65d7084f571e
AsherKikodze <asher.kikodze@gmail.com>
2021-05-07 14:08:43 +0300

    Converter

---
+++ src/ru/job4j/Calculator/MathFunc.java
@@ -0,0 +1,13 @@
+package ru.job4j.Calculator;
+
+public class MathFunc {
+    public static int func1(int x) {
+        return x * x + 1;
+    }
+    public static double func2(int x) {
+        return 1 / x;
+    }
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/ru/job4j/converter/Converter.java
@@ -0,0 +1,19 @@
+package ru.job4j.converter;
+
+public class Converter {
+    public static float rubleToEuro(float value) {
+        return value / 70;
+    }
+    public static float rubleToDollar(float value) {
+        return value / 60;
+    }
+
+    public static void main(String[] args) {
+        float rub = 140;
+        //Converter.rubleToEuro(rub);
+        System.out.println(rub + " rubles are " + Converter.rubleToEuro(rub)+ " euro");
+        System.out.println(rub + " rubles are " + Converter.rubleToDollar(rub)+ " USD");
+    }
+
+
+}
