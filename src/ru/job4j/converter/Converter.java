245f39d8ccb5bdfeadba12534e6765ad8d0501c4
AsherKikodze <asher.kikodze@gmail.com>
2021-05-09 14:25:37 +0300

    0. Что такое тестирование [#159431 #1048]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -6,7 +6,7 @@ public class Converter {
     }
 
     public static int rubleToDollar(int value) {
-        return -1;
+        return value / 60;
     }
 
     public static void main(String[] args) {
@@ -16,8 +16,8 @@ public class Converter {
         boolean passed = expected == out;
         System.out.println("140 rubles are 2 euro. Test result : " + passed);
         //Предположим, что реальный курс доллара 80
-        in = 240;
-        expected = 3;
+        in = 160;
+        expected = 2;
         passed = expected == Converter.rubleToDollar(in);
         System.out.println("160 rubles are 2 USD. Test result : " + passed);
     }
