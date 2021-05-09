0083bf302b8ea31002fbceae2469d789aa1f234b
AsherKikodze <asher.kikodze@gmail.com>
2021-05-09 13:09:11 +0300

    1.1.3. Тестирование  1. Модульные тесты

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -1,18 +1,18 @@
 package ru.job4j.converter;
 
 public class Converter {
-    public static float rubleToEuro(float value) {
+    public static int rubleToEuro(int value) {
         return value / 70;
     }
 
-    public static float rubleToDollar(float value) {
-        return value / 60;
+    public static int rubleToDollar(int value) {
+        return -1;
     }
 
     public static void main(String[] args) {
-        float in = 140;
-        float expected = 2;
-        float out = Converter.rubleToEuro(in);
+        int in = 140;
+        int expected = 2;
+        int out = Converter.rubleToEuro(in);
         boolean passed = expected == out;
         System.out.println("140 rubles are 2 euro. Test result : " + passed);
         //Предположим, что реальный курс доллара 80
