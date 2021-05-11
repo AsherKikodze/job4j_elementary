20a069f88f689aa6219743bd29445d25d3e2a417
AsherKikodze <asher.kikodze@gmail.com>
2021-05-11 08:37:52 +0300

    1.1.4. Оператор ветвления 4.6.1. Switch

---
+++ src/ru/job4j/condition/SwitchWeek.java
@@ -0,0 +1,39 @@
+package ru.job4j.condition;
+
+public class SwitchWeek {
+
+    public static String nameOfDay(int day) {
+        String res = "Ошибка";
+        switch (day) {
+            case 1:
+                res =  "Понедельник";
+                break;
+            case 2:
+                res =  "Вторник";
+                break;
+            case 3:
+                res =   "Среда";
+                break;
+            case 4:
+                res =   "Четверг";
+                break;
+            case 5:
+                res =   "Пятница";
+                break;
+            case 6:
+                res =   "Суббота";
+                break;
+            case 7:
+                res =   "Воскресенье";
+                break;
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        String dayWeek = SwitchWeek.nameOfDay(2);
+        System.out.println(dayWeek);
+
+    }
+}
