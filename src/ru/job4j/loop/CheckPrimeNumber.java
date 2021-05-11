5b34b9f55a8669a9c6f32dda91229cbf0e883388
AsherKikodze <asher.kikodze@gmail.com>
2021-05-11 20:32:38 +0300

    1.1.5. Циклы 5.5. Простое число [#171694 #1218]

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,16 +2,17 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        if (number == 1) {
+        if (number > 1) {
+            boolean prime = true;
+            for (int i = 2; i < number; i++) {
+                if ((number % i) == 0) {
+                    prime = false;
+                    break;
+                }
+            }
+            return prime;
+        } else {
             return false;
         }
-        boolean prime = true;
-        for (int i = 2; i < number; i++) {
-            if ((number % i) == 0) {
-                prime = false;
-                break;
-            }
-        }
-        return prime;
     }
 }
