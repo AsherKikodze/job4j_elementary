f12b0f8263e2334780f040095c2eb007190e789b
AsherKikodze <asher.kikodze@gmail.com>
2021-05-12 13:33:13 +0300

    1.1.6. Массивы 6.0.2. Заполнение массива. [#226300]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,9 +5,14 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
-
-        System.out.println("Размер массива ages равен: " + ages.length);
-        System.out.println("Размер массива surnames равен: " + surnames.length);
-        System.out.println("Размер массива prices равен: " + prices.length);
-    }
+        String[] names = new String[4];
+        names[0] = "Вася Пупкин";
+        names[1] = "Миша Данилин";
+        names[2] = "Витя Крылов";
+        names[3] = "Саша Мартынов";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+        }
 }
