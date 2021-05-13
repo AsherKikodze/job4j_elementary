149f402f7e12d7b5f4bf2781be1f7b5877c80b69
AsherKikodze <asher.kikodze@gmail.com>
2021-05-13 22:02:02 +0300

    1.1.6. Массивы 6.5.3. Сортировка выборкой. [#156320]

--- src/ru/job4j/array/SortSelected.java
+++ src/ru/job4j/array/SortSelected.java
@@ -2,11 +2,9 @@ package ru.job4j.array;
 
 public class SortSelected {
     public static int[] sort(int[] data) {
-        int val;
-        int num;
         for (int i = 0; i < data.length; i++) {
-            val = MinDiapason.findMin(data, i, data.length - 1);
-            num = FindLoop.indexOf(data, val, i, data.length - 1);
+            int val = MinDiapason.findMin(data, i, data.length - 1);
+            int num = FindLoop.indexOf(data, val, i, data.length - 1);
             data = SwitchArray.swap(data, i, num);
         }
         return data;
