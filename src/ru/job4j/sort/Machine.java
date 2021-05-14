1a9f346aed3c0b5e9ed1b61da36c242b2ae467d7
AsherKikodze <asher.kikodze@gmail.com>
2021-05-14 18:58:01 +0300

    1.1.6. Массивы  6.9. Жадный алгоритм. Сдача в кофе машине [#34741 #1364]

--- src/ru/job4j/sort/Machine.java
+++ src/ru/job4j/sort/Machine.java
@@ -3,13 +3,13 @@ package ru.job4j.sort;
 import java.util.Arrays;
 
 public class Machine {
-    public static int[] change(int money, int price) {
+    public static int[] change1(int money, int price) {
         int[] coins = {10, 5, 2, 1};
         int[] rsl = new int[100];
         int size = 0;
         int rem = money - price;
              for (int i = 0; i < coins.length; i++) {
-                 while (rem / coins[i] > 0) {
+                 while (rem > coins[i]) {
                      rsl[size] = coins[i];
                      size++;
                      rem -= coins[i];
@@ -17,4 +17,23 @@ public class Machine {
             }
          return Arrays.copyOf(rsl, size);
     }
+
+    public static int[] change(int money, int price) {
+        int[] coins = {10, 5, 2, 1};
+        int[] rsl = new int[100];
+        int size = 0;
+        int k = 0;
+        int rem = money - price;
+        while (rem > 0) {
+            if (rem >= coins[k]) {
+                rsl[size] = coins[k];
+                size++;
+                rem = rem - coins[k];
+            } else {
+                k++;
+            }
+        }
+        return Arrays.copyOf(rsl, size);
+    }
+
 }
