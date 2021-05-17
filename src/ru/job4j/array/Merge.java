7bf575f96578e2f0870bd38673bfa1cc79a037f4
AsherKikodze <asher.kikodze@gmail.com>
2021-05-17 19:37:51 +0300

    1.1.6. Массивы 3. Объединить два отсортированных массива

--- src/ru/job4j/array/Merge.java
+++ src/ru/job4j/array/Merge.java
@@ -1,41 +1,6 @@
 package ru.job4j.array;
 
 public class Merge {
-    public static  int[] merge2(int[] left, int[] right) {
-        int[] rsl = new int[left.length + right.length];
-        boolean condition = true;
-        int i = 0;
-        int j = 0;
-        int k = 0;
-        while (condition) {
-            if ((i < left.length) && (j < right.length)) { // Оба массива еще не пройдены
-                if (left[i] <= right[j]) { // Если значение в левом не больше правого, ставим левый
-                    rsl[k] = left[i];
-                    k++;
-                    i++;
-                } else { // Если значение в правом меньше левого, ставим девый
-                    rsl[k] = right[j];
-                    k++;
-                    j++;
-                }
-            } else if (i >= left.length) { // Пройден левый массив, дополняем остатками правого
-                for (int l = j; l < right.length; l++) {
-                    rsl[k] = right[l];
-                    k++;
-                }
-                condition = false;
-            } else { // Пройден правый массив, дополняем остатками левого
-                for (int l = i; l < left.length; l++) {
-                    rsl[k] = left[l];
-                    k++;
-                }
-                condition = false;
-            }
-
-        }
-        return rsl;
-    }
-
     public static  int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
         int i = 0;
@@ -44,22 +9,14 @@ public class Merge {
         while (k < left.length + right.length) {
             if ((i < left.length) && (j < right.length)) { // Оба массива еще не пройдены
                 if (left[i] <= right[j]) { // Если значение в левом не больше правого, ставим левый
-                    rsl[k] = left[i];
-                    k++;
-                    i++;
+                    rsl[k++] = left[i++];
                 } else { // Если значение в правом меньше левого, ставим левый
-                    rsl[k] = right[j];
-                    k++;
-                    j++;
+                    rsl[k++] = right[j++];
                 }
             } else if (j >= right.length) {
-                rsl[k] = left[i];
-                k++;
-                i++;
+                rsl[k++] = left[i++];
             } else {
-                rsl[k] = right[j];
-                k++;
-                j++;
+                rsl[k++] = right[j++];
             }
         }
         return rsl;
