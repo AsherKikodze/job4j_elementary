4c47e6caacb300a375c889aae7ecf01244ed82fa
AsherKikodze <asher.kikodze@gmail.com>
2021-05-26 13:59:12 +0300

    1.2.3. Инкапсуляция  3. Массивы и модели.

--- src/ru/job4j/pojo/Library.java
+++ src/ru/job4j/pojo/Library.java
@@ -5,7 +5,7 @@ public class Library {
         Book hemi = new Book("Старик и море", 30);
         Book plat = new Book("Сокровенный человек", 320);
         Book wolf = new Book("Домой возврата нет", 415);
-        Book clean = new Book("Clean code", 680);
+        Book clean = new Book("Clean code",680);
         Book[] lib = {hemi, plat, wolf, clean};
         for (Book i : lib) {
             System.out.println(i.getName() + " - " + i.getPages());
