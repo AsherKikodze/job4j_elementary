82a9696c2a7542dd45884bbc5687951601c0b45d
AsherKikodze <asher.kikodze@gmail.com>
2021-05-26 07:10:44 +0300

    1.2.3. Инкапсуляция 2. Модель данных. [#174683]

---
+++ src/ru/job4j/pojo/College.java
@@ -0,0 +1,18 @@
+package ru.job4j.pojo;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setFio("Jimmy Jumps");
+        student.setGroup("Marketing");
+        student.setDateEnter(new Date());
+        System.out.println(student.getFio());
+        System.out.println(student.getGroup());
+        System.out.println(student.getDateEnter());
+    }
+
+}
